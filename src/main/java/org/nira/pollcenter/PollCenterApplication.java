9be505f893177cb284e424c64c7b41059a1f8d63
AliNira <dev.erfaniii@gmail.com>
2026-06-04 16:05:28 -0800

    initial commit

---
+++ src/main/java/org/nira/pollcenter/PollCenterApplication.java
@@ -0,0 +1,13 @@
+package org.nira.pollcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nira/pollcenter/PollCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nira.pollcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
