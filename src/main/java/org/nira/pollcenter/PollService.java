1321344ca4aa0209e412aa69be1cffce4c48f2ff
AliNira <dev.erfaniii@gmail.com>
2026-06-06 19:46:24 -0800

    Add getPolls and getPoll APIs

--- src/main/java/org/nira/pollcenter/PollService.java
+++ src/main/java/org/nira/pollcenter/PollService.java
@@ -3,6 +3,9 @@ package org.nira.pollcenter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class PollService {
@@ -11,5 +14,13 @@ public class PollService {
 
     public Poll createPoll(Poll poll) {
         return pollRepo.save(poll);
+    }
+
+    public List<Poll> getPolls() {
+        return pollRepo.findAll();
+    }
+
+    public Optional<Poll> getPoll(Long id) {
+        return pollRepo.findById(id);
     }
 }
